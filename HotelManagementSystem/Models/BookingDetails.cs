649730901b6421cd63e5be31b95e8b69bd576b85
harshilvasani <vasani.h@husky.neu.edu>
2017-09-18 23:11:40 -0400

    First upload

---
+++ HotelManagementSystem/Models/BookingDetails.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace HotelManagementSystem.Models
+{
+    public class BookingDetails
+    {
+        static readonly string connectionString = "Server=localhost;Database=master;Trusted_connection = True;";
+        public ResultSet resultData = new ResultSet();
+        public string message = null;
+
+        public void GetBookingDetails(string confirmationCode)
+        {
+            if (confirmationCode == null)
+                return;
+
+            string queryString = @"SELECT DISTINCT b.confirmationCode, b.checkInDate, b.checkOutDate, b.roomType, b.numOfRoom, g.name, r.price*b.numOfRoom as 'Total Amt. Payed($)'
+	                                FROM master.[dbproject].[Booking] b, master.[dbproject].[Guest] g, master.[dbproject].[Room] r
+	                                WHERE b.confirmationCode = @confirmationCode
+		                            AND b.bookingOf = g.id
+		                            AND b.roomType = r.roomType";
+
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@confirmationCode", confirmationCode);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    command.Connection.Open();
+                    if (param != null)
+                        command.Parameters.AddRange(param);
+
+                    using (SqlDataReader myReader = command.ExecuteReader())
+                    {
+                        if (!myReader.HasRows)
+                        {
+                            message = "No booking found for the given confirmation code";
+                        }
+                        else
+                        {
+                            while (myReader.Read())
+                            {
+
+                                resultData.confirmationCode = myReader.GetString(0);
+                                resultData.checkinDate = myReader.GetDateTime(1);
+                                resultData.checkoutDate = myReader.GetDateTime(2);
+                                resultData.roomType = myReader.GetString(3);
+                                resultData.roomCount = myReader.GetInt32(4);
+                                resultData.guestName = myReader.GetString(5);
+                                resultData.amtPaid = myReader.GetDouble(6);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+
+        public int GetRoomCountCheckedIn(string confirmationCode)
+        {
+            if (string.IsNullOrEmpty(confirmationCode))
+                return 0;
+
+            string queryString = @" SELECT COUNT(*) as 'Number of rooms checked-In for given booking'
+                                    FROM [master].[dbproject].[CheckIn] ci JOIN [master].[dbproject].[Guest] g ON(ci.checkInBy = g.id)
+										JOIN [master].[dbproject].[Booking] b ON(b.bookingOf = g.id AND b.checkInDate = ci.checkInDate)
+										JOIN [master].[dbproject].[Room] r ON(ci.roomId = r.id)
+										WHERE b.confirmationCode = @confirmationCode;";
+
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@confirmationCode", confirmationCode);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    command.Connection.Open();
+                    if (param != null)
+                        command.Parameters.AddRange(param);
+
+                    using (SqlDataReader myReader = command.ExecuteReader())
+                    {
+                        if (myReader.Read())
+                        {
+
+                            return myReader.GetInt32(0);
+                        }
+
+                    }
+                }
+
+            }
+
+            return 0;
+        }
+
+    }
+
+    public class ResultSet
+    {
+        public string confirmationCode { get; set; }
+        public DateTime checkinDate { get; set; }
+        public DateTime checkoutDate { get; set; }
+        public string roomType { get; set; }
+        public int roomCount { get; set; }
+        public double amtPaid { get; set; }
+        public string guestName { get; set; }
+        public int roomCountCheckedIn { get; set; }
+    }
+}
---
+++ HotelManagementSystem/Models/SearchEmptyRoom.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace HotelManagementSystem.Models
+{
+    public class SearchEmptyRoom
+    {
+        static readonly string connectionString = "Server=localhost;Database=master;Trusted_connection = True;";
+        public List<int> resultData = new List<int>();
+        public string message = null;
+        public int totalRoomCount { get; set; }
+        public int checkedInRoomCount { get; set; }
+
+        public SearchEmptyRoom()
+        {
+
+        }
+
+        public SearchEmptyRoom(string confirmationCode, int count)
+        {
+            checkedInRoomCount = new BookingDetails().GetRoomCountCheckedIn(confirmationCode);
+            totalRoomCount = count;
+        }
+
+        public void GetEmptyRooms(string confirmationCode)
+        {
+            if (confirmationCode == null)
+                return;
+
+            string queryString = @"SELECT r.roomNumber as 'Available Room'
+	                                    FROM master.[dbproject].[Room] r, master.[dbproject].[OccupancyDates] od, master.[dbproject].[Booking] b
+	                                    WHERE b.confirmationCode = @confirmationCode
+	                                    AND r.roomType = b.roomType
+	                                    AND r.isOccupied = 0
+	                                    AND r.id = od.roomId
+	                                    AND od.inDate <= b.checkInDate
+	                                    AND od.outDate <= b.checkOutDate
+									UNION
+									SELECT r.roomNumber as 'Available Room'
+	                                    FROM master.[dbproject].[Room] r, master.[dbproject].[Booking] b
+									    WHERE b.confirmationCode = @confirmationCode
+	                                    AND r.isOccupied = 0
+									    AND r.roomType = b.roomType";
+
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@confirmationCode", confirmationCode);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    command.Connection.Open();
+                    if (param != null)
+                        command.Parameters.AddRange(param);
+
+                    using (SqlDataReader myReader = command.ExecuteReader())
+                    {
+                        while (myReader.Read())
+                        {
+                            resultData.Add(myReader.GetInt32(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        public void CheckInRoom(string confirmationCode, int roomNumber = 0)
+        {
+            if (confirmationCode == null && roomNumber == 0)
+                return;
+
+            string queryString = @"BEGIN TRANSACTION
+                                    UPDATE master.[dbproject].[OccupancyDates] 
+                                            SET roomId = (SELECT r.id FROM master.dbproject.Room r WHERE r.roomNumber = @roomNumber)
+                                            WHERE id in(
+                                          SELECT TOP(1) od.id FROM  [master].[dbproject].[OccupancyDates] od, [master].[dbproject].[Room] r1, [master].[dbproject].[Room] r2, [master].dbproject.Booking b
+                                            WHERE b.confirmationCode = @confirmationCode
+											AND b.checkInDate = od.inDate
+											AND b.checkOutDate = od.outDate
+											AND r1.roomNumber = @roomNumber /*room number from UI*/
+                                            AND r2.id = od.roomId  
+                                            AND r1.roomType = r2.roomType
+                                            AND r2.isOccupied = 0
+											AND not exists (select * from  [master].[dbproject].[OccupancyDates] od1
+											Where r1.id = od1.roomId))                                    
+
+                                    INSERT INTO master.[dbproject].CheckIn(roomId,checkInBy,checkInDate)
+                                          SELECT r.id, b.bookingOf, b.checkInDate
+                                          FROM master.[dbproject].[Booking] b JOIN master.[dbproject].[OccupancyDates] od ON ( od.inDate = b.checkInDate)
+                                          JOIN master.[dbproject].[Room] r ON (od.roomId = r.id)
+                                          WHERE b.confirmationCode = @confirmationCode
+                                          AND r.roomNumber = @roomNumber;
+
+                                    UPDATE master.[dbproject].Room
+                                          SET isOccupied = 1
+                                          WHERE roomNumber = @roomNumber;
+                                    COMMIT;";
+
+            SqlParameter[] param = new SqlParameter[2];
+            param[0] = new SqlParameter("@confirmationCode", confirmationCode);
+            param[1] = new SqlParameter("@roomNumber", roomNumber);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    command.Connection.Open();
+                    if (param != null)
+                        command.Parameters.AddRange(param);
+
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected == 0)
+                    {
+                        message = "Something went wrong while CheckIn";
+                    }
+                }
+            }
+        }
+    }
+}
